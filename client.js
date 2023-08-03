90f019aa06623a0ea935031358489f642543d989
Curtis Platt <curtisplatt@hotmail.com>
2023-08-03 12:35:24 -0400

    updating input, and all files

--- client.js
+++ client.js
@@ -1,1 +1,25 @@
-const connect = require("./play");
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  });
+  conn.on("connect", () => {
+    conn.write("Name: CMP");
+  });
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
+
+module.exports = { connect }
--- play.js
+++ play.js
@@ -1,43 +1,7 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    console.log("Successfully connected to game server");
-  });
-  conn.on("connect", () => {
-    conn.write("Name: CMP");
-  });
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
-
-const handleUserInput = function(key) {
-  if (key === "\u0003") {
-    process.exit();
-  }
-};
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
 
-module.exports = { connect }
+setupInput();
