f2fa7a4db3d09253cf20f483f6bb9d95b31de39f
Curtis Platt <curtisplatt@hotmail.com>
2023-08-03 10:55:24 -0400

    play and client snake connecter module

--- play.js
+++ play.js
@@ -1,21 +1,22 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
-    host: '165.227.47.243',
+    host: "165.227.47.243",
     port: 50541
-  });
-
-  conn.on("connect", () => {
-    console.log("you ded cuz you idled");
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  });
 
   return conn;
 };
 
 console.log("Connecting ...");
 connect();
+
+module.exports = { connect }
